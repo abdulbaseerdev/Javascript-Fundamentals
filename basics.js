bf0a7f9b11270671bc86eed63436e8e874f4d488
Salman Ghouri <121509813+abdulbaseerdev@users.noreply.github.com>
2022-12-27 22:18:50 +0500

    initial commit
    initial commit

---
+++ basics.js
@@ -0,0 +1,10 @@
+// 1- Alert
+// alert("I am a programmer");
+
+// 2- DOM FUNCTIONSgetElementById() 1.
+
+function baseer(){
+document.getElementById("baseerid").innerHTML = "My name is salman"
+}
+
+
